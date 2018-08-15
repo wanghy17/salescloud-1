18cd14aa96ef951261f977ae1c349148ffca08dc
qiumj <qiumingjie126@163.com>
2018-08-15 13:35:46 +0800

    salescloud init

---
+++ demo1/src/main/java/com/example/demo1/HellWorld.java
@@ -0,0 +1,21 @@
+package com.example.demo1;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/hell")
+public class HellWorld {
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    @ResponseBody
+    public Object sayHello(@RequestParam("name")String name){
+        if (null == name || name.trim().equals(""))
+            return "hello null sir.";
+        return "hello 2 " + name;
+    }
+}
---
+++ registercenter/src/main/java/com/yonyou/sales/cloud/registercenter/RegistercenterApplication.java
@@ -0,0 +1,14 @@
+package com.yonyou.sales.cloud.registercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistercenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistercenterApplication.class, args);
+	}
+}
