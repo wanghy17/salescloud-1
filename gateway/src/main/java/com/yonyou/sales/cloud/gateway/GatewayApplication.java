bbef486b01520da63482bee347c0bf2dba8deeda
wanghy17 <42170160+wanghy17@users.noreply.github.com>
2018-08-15 15:49:45 +0800

    Update GatewayApplication.java

--- gateway/src/main/java/com/yonyou/sales/cloud/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/yonyou/sales/cloud/gateway/GatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
